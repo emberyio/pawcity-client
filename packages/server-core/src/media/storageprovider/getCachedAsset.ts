6498e8b0256f3a02297607992a7938f065953755
An <me@clgt.io>
2021-12-18 11:05:38 +0700

    fix cache domain invalid

--- packages/server-core/src/media/storageprovider/getCachedAsset.ts
+++ packages/server-core/src/media/storageprovider/getCachedAsset.ts
@@ -1,4 +1,8 @@
+const pathModule = require('path')
+
 export const getCachedAsset = (path: string, cacheDomain: string) => {
   if (!cacheDomain) throw new Error('No cache domain found - please check the storage provider configuration')
-  return new URL(path ?? '', 'https://' + cacheDomain).href
+  const url = new URL('https://' + cacheDomain)
+  url.pathname = pathModule.join(url.pathname, path ?? '')
+  return url.href
 }
